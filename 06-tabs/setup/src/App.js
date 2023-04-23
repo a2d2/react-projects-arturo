5a360745498b2f2b0cf66cacd5dfd0b96b85f193
r2d2 <arturo.galvis@hotmail.com>
2023-04-23 10:44:47 -0500

    1

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,10 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = 'https://course-api.com/react-tabs-project';
 function App() {
-  return <h2>tabs project setup</h2>
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+  //current item
+  const fecthJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = response.json();
+    setJobs(newJobs);
+    setisLoading(false);
+  };
+
+  useEffect(() => {
+    fecthJobs();
+  }, []);
+  console.log(jobs);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className='"Loading'></div>
+      </section>
+    );
+  }
+  return <h2>Tabs Starter</h2>;
 }
 
-export default App
+export default App;
